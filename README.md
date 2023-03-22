bb0d0263a2dd8d1338c47677ca430db042ede039
marianamaher <mariana.maherr@gmail.com>
2023-03-22 18:22:18 -0400

    created the skeleton of the code
    ✓ Flask server
    ✓ html for index and layout
    ✓ main CSS file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # budgie
+
+@Mari
+✓ Created flask server: to run the app type: python server.py in the terminal
+✓ Created html for index and layout
+✓ Created main CSS file
--- server.py
+++ server.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 # ROUTES
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    return render_template('index.html')  
-
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug=True)
