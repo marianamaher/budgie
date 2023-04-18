553d3b56adf793c3948b805f6211db2fa01558e2
marianamaher <mariana.maherr@gmail.com>
2023-04-18 19:55:18 -0400

    updating routes

--- static/index.js
+++ static/index.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
     $("#generateBudgetButton").click(function() {
 
-        window.location.href = "http://127.0.0.1:5000/budgetPage";
+        window.location.href = "/budgetPage";
 
     })
 
     $("#savingTipsButton").click(function() {
 
-        window.location.href = "http://127.0.0.1:5000/tipsPage";
+        window.location.href = "/tipsPage";
 
 
     })
