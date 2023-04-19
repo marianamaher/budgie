f6a55b65a23f7d258e016fba78f25c23f90e4edc
marianamaher <mariana.maherr@gmail.com>
2023-04-18 20:18:13 -0400

    updating apikey environment

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from flask import render_template
 from flask import Response, request, jsonify
 import os
 app = Flask(__name__)
-openai.api_key = os.environ.get('SECRET_KEY', 'API_KEY')
+openai.api_key = os.environ.get('API_KEY')
 
 # ROUTES
 
